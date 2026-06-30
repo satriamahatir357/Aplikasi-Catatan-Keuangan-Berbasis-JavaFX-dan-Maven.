e1f713dfb40a1c6f140c82f5d2086e39531fe66a
satriamahatir357 <annasandhika019@gmail.com>
2026-06-30 22:49:29 +0700

    Menambahkan grafik batang pada dashboard keuangan

--- src/main/java/frontend/view/DashboardView.java
+++ src/main/java/frontend/view/DashboardView.java
@@ -3,6 +3,10 @@ package frontend.view;
 import backend.DashboardService;
 import backend.TransaksiService;
 import javafx.geometry.Pos; // Pos adalah sebuah kelas dalam JavaFX yang digunakan untuk menentukan posisi elemen dalam layout.
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.scene.control.Label; // Label adalah sebuah kelas dalam JavaFX yang digunakan untuk menampilkan teks statis pada antarmuka pengguna. Label biasanya digunakan untuk memberikan informasi atau deskripsi kepada pengguna.
 import javafx.scene.layout.HBox; // HBox adalah sebuah kelas dalam JavaFX yang digunakan untuk mengatur tata letak elemen-elemen secara horizontal. HBox memungkinkan Anda untuk menempatkan elemen-elemen di dalamnya secara berurutan dari kiri ke kanan.
 import javafx.scene.layout.VBox; // VBox adalah sebuah kelas dalam JavaFX yang digunakan untuk mengatur tata letak elemen-elemen secara vertikal. VBox memungkinkan Anda untuk menempatkan elemen-elemen di dalamnya secara berurutan dari atas ke bawah.
@@ -13,13 +17,16 @@ import java.util.Locale;
 public class DashboardView extends VBox { // DashboardView adalah sebuah kelas yang merupakan turunan dari VBox, yang digunakan untuk membuat tampilan dashboard dalam aplikasi. DashboardView akan menampilkan informasi seperti total pemasukan, total pengeluaran, dan saldo kepada pengguna.
 
         // field class
+        private final DashboardService dashboardService;
+        private TransaksiService transaksiService;
+
         private Label pemasukanValue;
         private Label pengeluaranValue;
         private Label saldoValue;
-        private TransaksiService transaksiService;
-        private final DashboardService dashboardService;
         private Label jumlahTransaksiValue;
         private Label rataRataValue;
+
+        private BarChart<String, Number> chart;
 
     public DashboardView(TransaksiService transaksiService) {
         this.transaksiService = transaksiService;
@@ -122,9 +129,28 @@ public class DashboardView extends VBox { // DashboardView adalah sebuah kelas y
         getStyleClass().add("dashboard-view");
         
         dashboardCards.getStyleClass().add("card-container");
+
+        // === Buat chart ===
+        // Buat sumbu X untuk kategori, buat sumbu Y untuk angka, lalu gunakan kedua sumbu tersebut untuk membuat sebuah diagram batang (BarChart)
+        CategoryAxis xAxis = new CategoryAxis(); // Membuat sumbu X (horizontal) yang berisi kategori atau teks.
+        NumberAxis yAxis = new NumberAxis(); // Membuat sumbu Y (vertikal) yang berisi angka.
+
+        chart = new BarChart<>(xAxis, yAxis); // Buat sebuah diagram batang menggunakan: xAxis sebagai sumbu horizontal, yAxis sebagai sumbu vertikal
+        chart.setTitle("Grafik Keuangan");
+        chart.setLegendVisible(false);
+        chart.setAnimated(false);
+        chart.setPrefHeight(300);
+
+        // Masukkan chart ke layout
+        VBox content = new VBox(
+            dashboardCards,
+            chart
+        );
+
+        content.setSpacing(10);
         
         // Masukkan card ke DashboardView
-        getChildren().add(dashboardCards);
+        getChildren().add(content);
 
         // memanggil method refreshDashboard
         refreshDashboard();
@@ -158,5 +184,29 @@ public class DashboardView extends VBox { // DashboardView adalah sebuah kelas y
         jumlahTransaksiValue.setText(
             dashboardService.getJumlahTransaksi() + " Transaksi"
         );
+
+        updateChart();
+    }
+
+    public void updateChart() {
+        chart.getData().clear();
+        XYChart.Series<String, Number> series = new XYChart.Series<>();
+
+        series.getData().add(
+            new XYChart.Data<>("Pemasukan",
+                dashboardService.getTotalPemasukan())
+        );
+
+        series.getData().add(
+            new XYChart.Data("Pengeluaran",
+                dashboardService.getTotalPengeluaran())
+        );
+
+        series.getData().add(
+            new XYChart.Data<>("Saldo",
+                dashboardService.getSaldo())
+        );
+
+        chart.getData().add(series);
     }
 }
